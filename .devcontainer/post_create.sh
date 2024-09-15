43dfd88cc24481c0f215f9c0395e900ee90f8115
Anton Jahn <54476893+antonjahn@users.noreply.github.com>
2024-09-15 09:12:25 +0200

    fix: path and permissions for post create scripts

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -6,4 +6,4 @@ sudo apt-get install -y build-essential texinfo file bison gawk
 
 # Make sure we're using bash
 echo "dash dash/sh boolean false" | sudo debconf-set-selections
-sudo dpkg-reconfigure dash
+sudo DEBIAN_FRONTEND=noninteractive dpkg-reconfigure dash
