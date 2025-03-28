600556e2b522f56633243d933a70bd87d67c6338
Anton Jahn <54476893+antonjahn@users.noreply.github.com>
2025-03-28 13:22:25 +0100

    Add missing package for build and bigpicture

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -2,7 +2,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 # Install build tools
 sudo apt-get update
-sudo apt-get install -y build-essential texinfo file bison gawk
+sudo apt-get install -y build-essential texinfo file bison gawk bubblewrap graphviz
 
 # Make sure we're using bash
 echo "dash dash/sh boolean false" | sudo debconf-set-selections
