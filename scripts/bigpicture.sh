a552adf973bc6fe8cd2379e31fc7fb4509d32c5c
Anton Jahn <54476893+antonjahn@users.noreply.github.com>
2025-04-05 21:29:44 +0000

    make big picture stable

--- scripts/bigpicture.sh
+++ scripts/bigpicture.sh
@@ -3,5 +3,5 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $SCRIPT_DIR/..
 
-bazel query 'deps(//...)' --noimplicit_deps --notool_deps --output=graph | grep -v '_src.tar' >doc/bigpicture.dot
+bazel query 'deps(//...)' --order_output full --noimplicit_deps --notool_deps --output=graph | grep -v '_src.tar' >doc/bigpicture.dot
 dot -Tsvg doc/bigpicture.dot -o doc/bigpicture.svg
